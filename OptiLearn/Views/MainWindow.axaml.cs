2f99586660423632985a0d74efc392b46b0cec41
Zexsoft <serbanp733@gmail.com>
2022-11-25 17:11:44 +0200

    Add project files.

---
+++ OptiLearn/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace OptiLearn.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
